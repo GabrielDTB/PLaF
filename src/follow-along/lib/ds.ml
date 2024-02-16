cf42b76042b8bba38e0f8774e6118ae518a1de36
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-02-16 13:58:16 -0500

    add >>= (bind) operator

--- src/follow-along/lib/ds.ml
+++ src/follow-along/lib/ds.ml
@@ -7,3 +7,9 @@ let return : 'a -> 'a result =
 
 let error : string -> 'a result =
   fun s -> Error s
+
+let (>>=) : 'a result -> ('a -> 'b result) -> 'b result =
+  fun c f ->
+  match c with
+  | Error s -> Error s
+  | Ok v -> f v
