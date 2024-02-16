665aaf54729cd95a36329d32249e853940246825
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-02-16 13:56:53 -0500

    add eval_expr function

--- src/follow-along/lib/interp.ml
+++ src/follow-along/lib/interp.ml
@@ -1,3 +1,21 @@
 open Ds
 open Parser_plaf.Ast
 open Parser_plaf.Parser
+    
+let rec eval_expr : expr -> int result =
+  fun e ->
+  match e with
+  | Int(n) -> Ok n
+  | Sub(e1, e2) -> (match eval_expr e1 with
+    | Error s -> Error s
+    | Ok m -> (match eval_expr e2 with
+      | Error s -> Error s
+      | Ok n -> Ok (m-n)))
+  | Div(e1, e2) -> (match eval_expr e1 with
+    | Error s -> Error s
+    | Ok m -> (match eval_expr e2 with
+      | Error s -> Error s
+      | Ok n -> if n == 0
+        then Error "Division by zero"
+        else Ok (m/n)))
+  | _ -> Error "Not yet implemented"
