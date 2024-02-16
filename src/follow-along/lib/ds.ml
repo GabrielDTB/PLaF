2244b417a48abbc65844c9405dbae794f8bdde06
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-02-16 13:57:21 -0500

    add return and error functions

--- src/follow-along/lib/ds.ml
+++ src/follow-along/lib/ds.ml
@@ -1,3 +1,9 @@
 (* Environment Abstracted Result *)
 
 type 'a result = Ok of 'a | Error of string
+
+let return : 'a -> 'a result =
+  fun v -> Ok v
+
+let error : string -> 'a result =
+  fun s -> Error s
