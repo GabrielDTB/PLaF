abc7e61b5ff2dd3deb613bb3bf87768b0d7e28e1
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-02-16 19:40:10 -0500

    add environment support

--- src/follow-along/lib/ds.ml
+++ src/follow-along/lib/ds.ml
@@ -17,3 +17,22 @@ let (>>=) : 'a result -> ('a -> 'b result) -> 'b result =
 type exp_val =
   | NumVal of int
   | BoolVal of bool
+
+type env =
+  | EmptyEnv
+  | ExtendEnv of string*exp_val*env
+
+let empty_env : unit -> env =
+  fun () -> EmptyEnv
+
+let extend_env : env -> string -> exp_val -> env =
+  fun env id v -> ExtendEnv(id, v, env)
+
+let rec apply_env : string -> env -> exp_val result =
+  fun id env ->
+  match env with
+  | EmptyEnv -> error (id ^ " not found")
+  | ExtendEnv(v, ev, tail) ->
+    if id = v
+    then return ev
+    else apply_env id tail
